27c8ccc3a8610fd7df10c31b3acfeecaef87faa6
Meenu Sowjanya Chandrasekaran <mchandrasekaran@fwsa.freshworks.com>
2022-08-13 15:03:18 +0530

    Frontend of bot page

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -6,10 +6,10 @@ class Ticket < ApplicationRecord
   belongs_to :status
   has_many_attached :screenshots
   has_many :notes
-  VALID_NAME_REGEX = /\A[^0-9`!@#$%\^&*+_=]+\z/.freeze
-  validates :subject, presence: true, length: { minimum: 3, maximum: 100 }, format: { with: VALID_NAME_REGEX }
+  VALID_TEXT_REGEX = /\A[A-Za-z]+(?:\s[A-Za-z]+)*\z/.freeze
+  validates :subject, presence: true, length: { minimum: 3, maximum: 100 }, format: { with: VALID_TEXT_REGEX }
   validates :status_id, presence: true
   validates :priority_id, presence: true
   validates :agent, presence: true
-  validates :description, presence: true, length: { minimum: 3, maximum: 255 }, format: { with: VALID_NAME_REGEX }
+  validates :description, presence: true, length: { minimum: 3, maximum: 255 }, format: { with: VALID_TEXT_REGEX }
 end
