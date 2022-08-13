ef7b8269e4a6001406165049fed1c3f149cd6e71
Meenu Sowjanya Chandrasekaran <mchandrasekaran@fwsa.freshworks.com>
2022-08-13 20:06:59 +0530

    Configured actionmailer and smtp settings

--- Gemfile
+++ Gemfile
@@ -62,6 +62,7 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
 
+gem 'mail_form'
 gem 'pagy'
 
 gem 'rubocop', require: false
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -77,4 +77,13 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    user_name: 'zuppticketingapp@gmail.com',
+    password: 'gxkibmtbxumosznt',
+    address: 'smtp.gmail.com',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 end
