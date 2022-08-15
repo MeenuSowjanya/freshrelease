e6c67d0e3b9e44b3a7338549a563c0371e261705
Meenu Sowjanya Chandrasekaran <mchandrasekaran@fwsa.freshworks.com>
2022-08-15 13:42:16 +0530

    Tried adding domain in production.rb file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -120,5 +120,5 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.action_mailer.default_url_options = { host: 'zupp-ticket-app-test.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'zupp-ticket-app-meenu.herokuapp.com' }
 end
