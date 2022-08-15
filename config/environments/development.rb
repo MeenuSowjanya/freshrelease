a27cd154d45963d611a2f80cbb61a1ba524af00a
Meenu Sowjanya Chandrasekaran <mchandrasekaran@fwsa.freshworks.com>
2022-08-15 11:35:22 +0530

    Committed action mailer url settings for heroku setup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,11 +76,12 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'https://zupp-ticket-app-meenu.herokuapp.com/' }
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     user_name: 'zuppticketingapp@gmail.com',
     password: 'gxkibmtbxumosznt',
+    domain: 'https://zupp-ticket-app-meenu.herokuapp.com/'
     address: 'smtp.gmail.com',
     port: 587,
     authentication: :plain,
